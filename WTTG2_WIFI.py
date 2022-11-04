4ff0c063faea75a0edce6c240eabbc0cc98cbb00
Michał Jeske <michal.jes@gmail.com>
2022-11-04 01:58:03 +0100

    Formating update

--- WTTG2_WIFI.py
+++ WTTG2_WIFI.py
@@ -3,30 +3,34 @@ import time
 import os
 import winsound
 
-clear = lambda: os.system('cls')
 
-def time_calc(in_time,a):
-    hour=math.floor(in_time/100)
-    min=in_time%60
-    min=min+a
-    if min>60:
-        min=min%60
-        hour+=1
-    if hour>12:
-        hour=hour%12
+def clear(): return os.system('cls')
+
+
+def time_calc(in_time, a):
+    hour = math.floor(in_time/100)
+    min = in_time % 60
+    min = min+a
+    if min > 60:
+        min = min % 60
+        hour += 1
+    if hour > 12:
+        hour = hour % 12
     return hour*100+min
 
+
 def BEEP(iterations):
-    while iterations>0:
+    while iterations > 0:
         winsound.Beep(2000, 200)
         time.sleep(0.2)
-        iterations-=1
+        iterations -= 1
 
-def network_calc(ingame_t,network_time):
-    end_time=time_calc(ingame_t,network_time)
-    if end_time<1000:
-        end_time=str(0)+str(end_time)
-    end_time_S=str(end_time)
+
+def network_calc(ingame_t, network_time):
+    end_time = time_calc(ingame_t, network_time)
+    if end_time < 1000:
+        end_time = str(0)+str(end_time)
+    end_time_S = str(end_time)
     print("Start: " + str(ingame_t) + " End: " + end_time_S)
     time.sleep((network_time-2)*60)
     BEEP(2)
@@ -36,18 +40,19 @@ def network_calc(ingame_t,network_time):
     BEEP(4)
     print("!!! Disconnect !!!")
 
-def siec(ingame_time,network_number):
+
+def siec(ingame_time, network_number):
     match network_number:
         case 1:
-            network_calc(ingame_time,32)
+            network_calc(ingame_time, 32)
         case 2:
-            network_calc(ingame_time,16)
+            network_calc(ingame_time, 16)
         case 3:
-            network_calc(ingame_time,14)
+            network_calc(ingame_time, 14)
         case 4:
-            network_calc(ingame_time,21)
+            network_calc(ingame_time, 21)
         case 5:
-            network_calc(ingame_time,10)
+            network_calc(ingame_time, 10)
         case _:
             print("!!! Błąd, zła sieć !!!")
             time.sleep(2)
@@ -62,5 +67,5 @@ while True:
     print("4: do")
     print("5: Fr")
     print("________________________________")
-    UserIn=list(map(int, input().split()))
-    siec(UserIn[0],UserIn[1])
+    UserIn = list(map(int, input().split()))
+    siec(UserIn[0], UserIn[1])
